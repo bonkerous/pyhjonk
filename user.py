070edab8919aacc7b5bb6c8e8a5ffe557ca499a4
bonkerous <rczj@yahoo.com>
2025-02-26 21:47:42 +0000

    Making some progress on the HTML parser, however I seem to have broke the pulling because it shows the same posts regardless of who's username you put in. Too bad! I'll fix it later :)

--- user.py
+++ user.py
@@ -1,9 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 base_url = "https://hjonk.me"
 
 username = input("Enter the username of the user you want the posts from: ")
 print("You chose to view posts from", username)
 
-responce = requests.get(f"{base_url}/profile/feed?user={username}")
-print(responce.content)
+response = requests.get(f"{base_url}/profile/feed?user={username}")
+
+# Stuff to parse HTML
+soup = BeautifulSoup(response.content, 'html5lib')
+
+posts_text_raw = soup.find_all("div", class_="text")
+
+for posts_text in posts_text_raw:
+    print(posts_text.text)
