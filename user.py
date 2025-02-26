7561904c860a83f80d68b8a8f705985604260336
joel <rczj@yahoo.com>
2025-02-26 21:33:14 +0100

    add very undone user.py to repo
    it can get the feed html from the site and thats it, should probably work on parsing…
    ps. maybe we’ll get an api tho so thats nice

---
+++ user.py
@@ -0,0 +1,10 @@
+import requests
+
+base_url = "https://hjonk.me"
+
+username = input("Enter the username of the user you want the posts from: ")
+print("You chose to view posts from", username)
+
+responce = requests.get(f"{base_url}/profile/feed?user={username}")
+#print(responce.content)
+
