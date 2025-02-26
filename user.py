d2e4db5e68f41f3270ad666afba1bfb1f145ecdf
joel <rczj@yahoo.com>
2025-02-26 21:35:03 +0100

    Update user.py
    whoopsies forgot to uncomment the print function after messing around :3

--- user.py
+++ user.py
@@ -6,5 +6,4 @@ username = input("Enter the username of the user you want the posts from: ")
 print("You chose to view posts from", username)
 
 responce = requests.get(f"{base_url}/profile/feed?user={username}")
-#print(responce.content)
-
+print(responce.content)
