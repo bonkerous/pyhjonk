cb41a3987eb3a9b5c2e36ecd3970c9d817134813
Joel <rczj@yahoo.com>
2025-02-27 22:10:41 +0100

    Added cool stuff that makes user.py actually pull from given users posts Basically you put your handle and password into the thing and it just logs in and that saves cookies so yea

--- user.py
+++ user.py
@@ -2,11 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 base_url = "https://hjonk.me"
+session = requests.Session()
+
+payload = {'handle': "YOURHANDLE", 'password': "YOURPASSWORD"}
+
+session.post(f"{base_url}/auth/login", data=payload)
+
+print(session)
 
 username = input("Enter the username of the user you want the posts from: ")
 print("You chose to view posts from", username)
 
-response = requests.get(f"{base_url}/profile/feed?user={username}")
+response = session.get(f"{base_url}/profile/feed?user={username}")
 
 # Stuff to parse HTML
 soup = BeautifulSoup(response.content, 'html5lib')
