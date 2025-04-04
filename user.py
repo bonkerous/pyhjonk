d00175bbf26c19986b23747fed28a25ff6e36ce8
Bonkerous <rczj@yahoo.com>
2025-04-04 21:55:19 +0200

    The arguments update
    Add commandline arguments with argparse, very nice

--- user.py
+++ user.py
@@ -1,9 +1,22 @@
+import sys
+
 import requests
 from dotenv import load_dotenv
 import os
 import json
+import argparse
 
 load_dotenv()
+
+cmdparser = argparse.ArgumentParser(
+    description="pyhjonk user.py - View posts from a user.",
+    epilog="usage: py [-h] [args] username"
+)
+
+cmdparser.add_argument("username", type=str, default=1, help="Selects username")
+cmdparser.add_argument("-p", "--page", type=int, default=1, help="Selects page (Default: 1)")
+cmdparser.add_argument("-r", "--rows", type=int, default=10, help="Selects amounts of rows to get (Default: 10)")
+cmdargs=cmdparser.parse_args()
 
 url = "https://hjonk.me"
 session = requests.Session()
@@ -16,10 +29,7 @@ if login.status_code == 200:
 else:
     print("Are you sure your credentials are correct?")
 
-username = input("Enter the username of the user you want the posts from: ")
-print(f"You chose to view posts from {username}")
-
-posts = session.get(f"{url}/api/v1.0/posts/{username}")
+posts = session.get(f"{url}/api/v1.0/posts/{cmdargs.username}?lim={cmdargs.rows}&page={cmdargs.page}")
 
 for item in posts.json():
     print(f"{item['body']} (posted at {item['created_at']})")
